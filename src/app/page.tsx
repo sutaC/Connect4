08f596da0ffb57862fc8508f6de322bc92215a28
Catus <catus.pl@gmail.com>
2023-11-20 14:38:43 +0100

    Finish dialog view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,79 +7,77 @@ import styles from "./page.module.css";
 import { useState } from "react";
 
 export default function Home() {
-	const [modalOpen, setModalOpen] = useState(false);
+    const [modalOpen, setModalOpen] = useState(false);
 
-	function closeModal() {
-		setModalOpen(false);
-	}
+    function closeModal() {
+        setModalOpen(false);
+    }
 
-	return (
-		<>
-			<main className={styles.main}>
-				<h1>Connect4</h1>
-				<div className={styles.menu}>
-					<div>
-						<Link href='/ai'>
-							<CustomButton>Play with AI</CustomButton>
-						</Link>
-					</div>
-					<div
-						onClick={() => {
-							setModalOpen(true);
-						}}
-					>
-						<CustomButton>Play online</CustomButton>
-					</div>
-					<div>
-						<Link href='/local'>
-							<CustomButton>Play locally</CustomButton>
-						</Link>
-					</div>
-				</div>
-			</main>
-			<CustomModal open={modalOpen}>
-				<header className={styles.modalHeader}>
-					<button
-						onClick={closeModal}
-						className={styles.modalCloseBtn}
-					>
-						x
-					</button>
+    return (
+        <>
+            <main className={styles.main}>
+                <h1>Connect4</h1>
+                <div className={styles.menu}>
+                    <div>
+                        <Link href="/ai">
+                            <CustomButton>Play with AI</CustomButton>
+                        </Link>
+                    </div>
+                    <div
+                        onClick={() => {
+                            setModalOpen(true);
+                        }}
+                    >
+                        <CustomButton>Play online</CustomButton>
+                    </div>
+                    <div>
+                        <Link href="/local">
+                            <CustomButton>Play locally</CustomButton>
+                        </Link>
+                    </div>
+                </div>
+            </main>
 
-					<h2>Play online games</h2>
-				</header>
+            <CustomModal open={modalOpen}>
+                <header className={styles.modalHeader}>
+                    <button onClick={closeModal} className={styles.modalBtn}>
+                        x
+                    </button>
 
-				<form className={styles.modalForm}>
-					<p>Join game</p>
-					<input
-						type='text'
-						name='gameCode'
-						placeholder='Game code...'
-						required
-						minLength={8}
-						maxLength={8}
-					/>
-					<CustomButton>Join</CustomButton>
-				</form>
+                    <h2>Play online games</h2>
+                </header>
 
-				<form className={styles.modalForm}>
-					<p>Host game</p>
-					<div className={styles.customCheckbox}>
-						<input
-							type='checkbox'
-							name='publicGame'
-							id='publicGame'
-						/>
-						<label htmlFor='publicGame'>Public game</label>
-					</div>
-					<CustomButton>Host game</CustomButton>
-				</form>
+                <form className={styles.modalForm}>
+                    <p>Join game</p>
+                    <input
+                        type="text"
+                        name="gameCode"
+                        placeholder="Game code..."
+                        required
+                        minLength={8}
+                        maxLength={8}
+                    />
+                    <CustomButton>Join</CustomButton>
+                </form>
 
-				<div>
-					<CustomButton>Find quick game</CustomButton>
-				</div>
-			</CustomModal>
-			<CustomFooter></CustomFooter>
-		</>
-	);
+                <form className={styles.modalForm}>
+                    <p>Host game</p>
+                    <div className={styles.customCheckbox}>
+                        <input
+                            type="checkbox"
+                            name="publicGame"
+                            id="publicGame"
+                        />
+                        <label htmlFor="publicGame">Public game</label>
+                    </div>
+                    <CustomButton>Host game</CustomButton>
+                </form>
+
+                <div>
+                    <CustomButton>Find quick game</CustomButton>
+                </div>
+            </CustomModal>
+            <CustomFooter></CustomFooter>
+        </>
+    );
 }
