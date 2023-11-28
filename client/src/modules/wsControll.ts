2ad058be63a71273924add8d4745582792c396a8
sutaC <catus.pl@gmail.com>
2023-11-28 09:33:55 +0100

    Refactor ws controll oo

--- client/src/modules/wsControll.ts
+++ client/src/modules/wsControll.ts
@@ -20,23 +20,70 @@ interface WsEventInit extends WsEvent {
     };
 }
 
-function getWsEvent(event: MessageEvent): WsEvent {
-    return JSON.parse(event.data);
+export default class WsControll {
+
+    private socket: WebSocket;
+
+    public onBoardUpdate?: (board: Player[][], turn: Player) => void;
+
+    constructor(socket: WebSocket, gameCode: number){
+        this.socket = socket;
+
+        this.sendMessage(this.socket, "userConnect", {gameCode})
+
+        socket.addEventListener("open", handleWsInit)
+
+    }
+
+    public getWsEvent(event: MessageEvent): WsEvent {
+        return JSON.parse(event.data);
+    }
+    
+    // ---
+    
+    public sendMessage(
+        socket: WebSocket,
+        event: string,
+        data: {}
+    ) {
+        const userConnectEvent: WsEvent = {
+            event,
+            payload: data,
+        };
+        socket.send(JSON.stringify(userConnectEvent));
+    }
+
+    // ---
+
+    private handleWsOpen(
+        event: MessageEvent,
+        socket: WebSocket,
+    ) {
+        const wsEvent = getWsEvent(event) as WsEventInit;
+        if (wsEvent.event !== "userAuth") {
+            return;
+        }
+    
+        switch (wsEvent.payload.status) {
+            case "error":
+                console.error("Couldn't initialize user", wsEvent.payload.msg);
+                return;
+            case "waiting":
+                console.warn("Waiting for other player", wsEvent.payload.msg);
+                return;
+            case "ready":
+                socket.removeEventListener("message", listener);
+                socket.addEventListener("message", (event) => {
+                    handleWsBoardUpdate(event, handleBoardUpdate);
+                });
+                if (succesCb) succesCb((wsEvent.payload.color ?? null) as Player);
+        }
+    }
+    
+
+
 }
 
-// Func
-
-export function wsSendUserConnect(
-    socket: WebSocket,
-    data: { gameCode: number }
-) {
-    const userConnectEvent: WsEvent = {
-        event: "userConnect",
-        payload: data,
-    };
-
-    socket.send(JSON.stringify(userConnectEvent));
-}
 
 function handleWsBoardUpdate(
     event: MessageEvent,
@@ -50,9 +97,6 @@ function handleWsBoardUpdate(
 export function handleWsInit(
     event: MessageEvent,
     socket: WebSocket,
-    listener: (event: MessageEvent) => void,
-    handleBoardUpdate: (board: Player[][], turn: Player) => void,
-    succesCb?: (color: Player) => void
 ) {
     const wsEvent = getWsEvent(event) as WsEventInit;
     if (wsEvent.event !== "userAuth") {
