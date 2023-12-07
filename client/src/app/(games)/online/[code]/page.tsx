41fd04b33beacf75dbaaa68308068ed68a9bbc94
Antoni Siekański <82384481+sutaC@users.noreply.github.com>
2023-12-07 13:41:52 +0100

    User gui for errors

--- client/src/app/(games)/online/[code]/page.tsx
+++ client/src/app/(games)/online/[code]/page.tsx
@@ -9,138 +9,157 @@ import WsControll from "@/modules/wsControll";
 import { BoardClickEvent } from "@/components/board";
 
 export default function Page() {
-    const wsControllRef: MutableRefObject<WsControll | undefined> = useRef();
-    let gameCode = 0;
+	const wsControllRef: MutableRefObject<WsControll | undefined> = useRef();
+	let gameCode = 0;
 
-    let board: Player[][] = getEmptyBoard();
-    let turn: Player = null;
-    let player: Player = null;
+	let board: Player[][] = getEmptyBoard();
+	let turn: Player = null;
+	let player: Player = null;
 
-    const [boardView, setBoardView] = useState(board);
-    const [turnMsg, setTurnMsg] = useState<string>(turn ?? "");
-    const [modalOpen, setModalOpen] = useState(true);
-    const [gameCodeView, setGameCodeView] = useState(gameCode);
+	const [boardView, setBoardView] = useState(board);
+	const [turnMsg, setTurnMsg] = useState<string>(turn ?? "");
 
-    // --- Game init ---
+	const [modalWaitingOpen, setModalWaitingOpen] = useState(true);
+	const [modalErrorOpen, setModalErrorOpen] = useState(false);
 
-    useEffect(() => {
-        // Get gamecode
-        try {
-            const devider = location.pathname.lastIndexOf("/");
-            gameCode = Number(location.pathname.substring(devider + 1));
-            setGameCodeView(gameCode);
-        } catch (error) {
-            console.error(error);
-        }
-    }, []);
+	const [gameCodeView, setGameCodeView] = useState(gameCode);
+	const [errorMsg, setErrorMsg] = useState("Unknown");
 
-    useEffect(() => {
-        // Get ws controll
-        if (wsControllRef.current) return;
+	// --- Game init ---
 
-        try {
-            const socket = new WebSocket("ws://localhost:3040");
-            wsControllRef.current = new WsControll(socket, gameCode);
-            wsControllRef.current.onWsAutentication = handleWsAthentication;
-            wsControllRef.current.onBoardUpdate = handleBoardUpdate;
-            wsControllRef.current.onWsError = handleWsError;
-            wsControllRef.current.onGameEnd = handleGameEnd;
-        } catch (error) {
-            console.error(error);
-        }
-    }, []);
+	useEffect(() => {
+		// Get gamecode
+		try {
+			const devider = location.pathname.lastIndexOf("/");
+			gameCode = Number(location.pathname.substring(devider + 1));
+			setGameCodeView(gameCode);
+		} catch (error) {
+			console.error(error);
+		}
+	}, []);
 
-    const boardClickeventListener = (e: Event) => {
-        handleBoardClick(e as BoardClickEvent);
-    };
-    useEffect(() => {
-        document.removeEventListener("boardClick", boardClickeventListener);
-        document.addEventListener("boardClick", boardClickeventListener);
-    }, []);
+	useEffect(() => {
+		// Get ws controll
+		if (wsControllRef.current) return;
 
-    // --- Game actions ---
+		try {
+			const socket = new WebSocket("ws://localhost:3040");
+			wsControllRef.current = new WsControll(socket, gameCode);
+			wsControllRef.current.onWsAutentication = handleWsAthentication;
+			wsControllRef.current.onBoardUpdate = handleBoardUpdate;
+			wsControllRef.current.onWsError = handleWsError;
+			wsControllRef.current.onGameEnd = handleGameEnd;
+		} catch (error) {
+			console.error(error);
+		}
+	}, []);
 
-    function handleBoardClick(event: BoardClickEvent) {
-        if (player !== turn || !player) return;
+	const boardClickeventListener = (e: Event) => {
+		handleBoardClick(e as BoardClickEvent);
+	};
+	useEffect(() => {
+		document.removeEventListener("boardClick", boardClickeventListener);
+		document.addEventListener("boardClick", boardClickeventListener);
+	}, []);
 
-        const { row } = event.detail;
+	// --- Game actions ---
 
-        if (!isMovePlayable(board, row)) return;
+	function handleBoardClick(event: BoardClickEvent) {
+		if (player !== turn || !player) return;
 
-        wsControllRef.current?.sendPlayerMoveEvent(row);
-    }
+		const { row } = event.detail;
 
-    function handleExit() {
-        if (
-            confirm(
-                "Are you sure you want to exit game? It will be lost forever!"
-            )
-        ) {
-            location.href = "/";
-        }
-    }
+		if (!isMovePlayable(board, row)) return;
 
-    function handleNewGame() {
-        wsControllRef.current?.sendNewGameEvent();
+		wsControllRef.current?.sendPlayerMoveEvent(row);
+	}
 
-        setModalOpen(true);
-    }
+	function handleExit() {
+		if (
+			confirm("Are you sure you want to exit game? It will be lost forever!")
+		) {
+			location.href = "/";
+		}
+	}
 
-    function handleWsAthentication(color: Player) {
-        player = color;
-        console.log("User authenticated as: " + color);
-    }
+	function handleNewGame() {
+		wsControllRef.current?.sendNewGameEvent();
 
-    function handleBoardUpdate(newBoard: Player[][], newTurn: Player) {
-        if (modalOpen) setModalOpen(false);
+		setModalWaitingOpen(true);
+	}
 
-        board = newBoard;
-        setBoardView(board);
+	function handleWsAthentication(color: Player) {
+		player = color;
+		console.log("User authenticated as: " + color);
+	}
 
-        turn = newTurn;
-        setTurnMsg((player === turn && turn ? "your" : "enemy's") + " turn!");
-    }
+	function handleBoardUpdate(newBoard: Player[][], newTurn: Player) {
+		if (modalWaitingOpen) setModalWaitingOpen(false);
 
-    function handleGameEnd(result: Player) {
-        let gameOverEvent: GameOverEvent;
-        if (result) {
-            gameOverEvent = new CustomEvent("gameOver", {
-                detail: { msg: `${result} won!` },
-            });
-        } else {
-            gameOverEvent = new CustomEvent("gameOver", {
-                detail: { msg: "Draw!" },
-            });
-        }
-        document.dispatchEvent(gameOverEvent);
-    }
+		board = newBoard;
+		setBoardView(board);
 
-    function handleWsError(msg: string, critical?: boolean) {
-        if (critical) console.error("CRITICAL::" + msg);
-        else console.warn(msg);
-        // TODO: User gui
-    }
+		turn = newTurn;
+		setTurnMsg((player === turn && turn ? "your" : "enemy's") + " turn!");
+	}
 
-    // --- Render ---
-    return (
-        <>
-            <GamePage
-                board={boardView}
-                turnMsg={turnMsg}
-                handleNewGame={handleNewGame}
-                handleExit={handleExit}
-            ></GamePage>
-            <CustomModal open={modalOpen}>
-                <h2>Waiting for other player...</h2>
-                <div>
-                    <small>Game code:</small>
-                    <p className="highlight">{gameCodeView ?? ""}</p>
-                </div>
-                <div onClick={handleExit}>
-                    <CustomButton>Exit</CustomButton>
-                </div>
-            </CustomModal>
-            <CustomFooter>Online multiplayer</CustomFooter>
-        </>
-    );
+	function handleGameEnd(result: Player) {
+		let gameOverEvent: GameOverEvent;
+		if (result) {
+			gameOverEvent = new CustomEvent("gameOver", {
+				detail: { msg: `${result} won!` },
+			});
+		} else {
+			gameOverEvent = new CustomEvent("gameOver", {
+				detail: { msg: "Draw!" },
+			});
+		}
+		document.dispatchEvent(gameOverEvent);
+	}
+
+	function handleWsError(msg: string, critical?: boolean) {
+		if (!critical) {
+			console.warn(msg);
+			return;
+		}
+
+		console.error("CRITICAL::" + msg);
+
+		setModalWaitingOpen(false);
+
+		setErrorMsg(msg);
+		setModalErrorOpen(true);
+	}
+
+	// --- Render ---
+	return (
+		<>
+			<GamePage
+				board={boardView}
+				turnMsg={turnMsg}
+				handleNewGame={handleNewGame}
+				handleExit={handleExit}
+			></GamePage>
+			<CustomModal open={modalWaitingOpen}>
+				<h2>Waiting for other player...</h2>
+				<div>
+					<small>Game code:</small>
+					<p className="highlight">{gameCodeView ?? ""}</p>
+				</div>
+				<div onClick={handleExit}>
+					<CustomButton>Exit</CustomButton>
+				</div>
+			</CustomModal>
+			<CustomModal open={modalErrorOpen}>
+				<h2>Error occured!</h2>
+
+				<p className="highlight">{errorMsg}</p>
+
+				<div onClick={handleExit}>
+					<CustomButton>Exit</CustomButton>
+				</div>
+			</CustomModal>
+			<CustomFooter>Online multiplayer</CustomFooter>
+		</>
+	);
 }
